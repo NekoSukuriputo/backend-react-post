77828a26c8be2c2a0bc84d497955125fc0f81c7f
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-02 21:53:32 +0700

    fixing import

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { getStoredPosts, storePosts } = require('./data/posts');
+const { getStoredPosts, storePosts } = require('../data/posts');
 
 const app = express();
 
