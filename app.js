b118f60387d41f880b90ddd724f16f6a2b132d36
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-07-02 21:40:32 +0700

    addicng static

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { getStoredPosts, storePosts } = require('./data/posts');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
